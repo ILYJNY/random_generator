4802fec62567aee1abd8b6a93f4c43ed1c5cf78c
HinaLover <164602079+ILYJNY@users.noreply.github.com>
2024-05-03 18:00:50 +0900

    Update random_custom.cpp

--- random_custom.cpp
+++ random_custom.cpp
@@ -4,14 +4,16 @@
 
 #include "random"
 #include "iostream"
+#define lf long double
+
 
 using namespace std;
 
-unsigned long long int random_custom(long double m, long double n) {
+lf random_custom(lf m, lf n) {
     random_device rd;//시드값을위한 random_device 생성
     mt19937 gen(rd());//random_device를 통해 난수 생성 엔진을 초기화한다
-    uniform_int_distribution<unsigned long long int> dis((unsigned long long int) m, (unsigned long long int) n);//균등분포 정의
-    auto result = (unsigned long long int) dis(gen);
-    //printf("%s %llu\n","result_random : " ,result);
+    uniform_int_distribution<unsigned long long int> dis((lf) m, (lf) n);//균등분포 정의
+    auto result = (lf) dis(gen);
+    //printf("%s %Lf\n","result_random : " ,result);
     return result;
 }
